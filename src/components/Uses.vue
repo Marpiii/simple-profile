59346787b1b60e3a8b38ad3ff19e15863f48f0c3
Marpiii <andrianskys@gmail.com>
2025-10-04 09:35:00 +0700

    Update Uses.vue

--- src/components/Uses.vue
+++ src/components/Uses.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="mb-2 font-black text-2xl">uses/</div>
-  <img src="https://skillicons.dev/icons?i=linux,git,docker,vscode,github,mongodb" class="select-none mb-2" />
-  <img src="https://skillicons.dev/icons?i=c,python,go,js,tailwind,react,vuejs,nextjs" class="select-none" />
+  <img src="https://skillicons.dev/icons?i=instagram,unity,vscode,discord,windows,gmail,github" class="select-none mb-2" />
+  <img src="https://skillicons.dev/icons?i=java,js,firebase,cs,c,cpp,html,lua,php" class="select-none" />
 </template>
