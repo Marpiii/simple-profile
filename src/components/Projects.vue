9cb1b0a2ecd1f3e7d0a9df60b082f20fd105096e
Abraham <andrianskys@gmail.com>
2025-10-09 21:34:49 +0700

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -14,15 +14,15 @@
       </div>
 
       <div>
-        <div class="font-bold text-lg">Other Profile </div>
-        <div class="text-gray-300">Website Paling Simple & Praktis.</div>
-        <div class="mt-2 text-xs text-gray-500">⭐ 100 &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 100</div>
+        <div class="font-bold text-lg">MAIN PROFILE</div>
+        <div class="text-gray-300">An website profile of me.</div>
+        <div class="mt-2 text-xs text-gray-500">★★★★☆</div>
       </div>
     </a>
 
     <!-- Project: siker.im -->
     <a
-      href="https://e-z.bio/marv"
+      href="https://chat.whatsapp.com/LGyajryoaB5BIZaZyQMUzW?mode=ems_copy_t"
       target="_blank"
       class="flex items-start gap-3 px-4 py-3 bg-[#202020]/30 border border-[#504945] rounded-lg text-sm hover:bg-[#2a2a2a] transition"
     >
@@ -31,44 +31,9 @@
       </div>
 
       <div>
-        <div class="font-bold text-lg">Project Kedua</div>
-        <div class="text-gray-300">Banyak Nanya lu.</div>
-        <div class="mt-2 text-xs text-gray-500">⭐ 7 &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 2</div>
+        <div class="font-bold text-lg">MarV Script</div>
+        <div class="text-gray-300">Click disini untuk mengambil scriptnya.</div>
+        <div class="mt-2 text-xs text-gray-500">★★★☆☆</div>
       </div>
     </a>
-
-    <!-- Project: FdF -->
-    <a
-      href="https://e-z.bio/marv"
-      target="_blank"
-      class="flex items-start gap-3 px-4 py-3 bg-[#202020]/30 border border-[#504945] rounded-lg text-sm hover:bg-[#2a2a2a] transition"
-    >
-      <div class="w-10 h-10 rounded-full bg-[#333] flex items-center justify-center text-xl">
-        🧱
-      </div>
-
-      <div>
-        <div class="font-bold text-lg">Project ketiga</div>
-        <div class="text-gray-300">ga tau males gua.</div>
-        <div class="mt-2 text-xs text-gray-500">⭐ 5 &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 0</div>
-      </div>
-    </a>
-
-    <!-- Project: cub3D -->
-    <a
-      href="https://e-z.bio/marv"
-      target="_blank"
-      class="flex items-start gap-3 px-4 py-3 bg-[#202020]/30 border border-[#504945] rounded-lg text-sm hover:bg-[#2a2a2a] transition"
-    >
-      <div class="w-10 h-10 rounded-full bg-[#333] flex items-center justify-center text-xl">
-        🎮
-      </div>
-
-      <div>
-        <div class="font-bold text-lg">Malas</div>
-        <div class="text-gray-300">Kalcer abis.</div>
-        <div class="mt-2 text-xs text-gray-500">⭐ 3 &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 1</div>
-      </div>
-    </a>
-  </div>
 </template>
