7c2c46df34ea9093fa878f2e877a8b260eedcdf9
Abraham <andrianskys@gmail.com>
2025-10-09 21:38:22 +0700

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -16,7 +16,7 @@
       <div>
         <div class="font-bold text-lg">MAIN PROFILE</div>
         <div class="text-gray-300">An website profile of me.</div>
-        <div class="mt-2 text-xs text-gray-500">★★★★☆</div>
+        <div class="mt-2 text-xs text-gray-500">★★★★☆ &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 0</div>
       </div>
     </a>
 
@@ -33,7 +33,42 @@
       <div>
         <div class="font-bold text-lg">MarV Script</div>
         <div class="text-gray-300">Click disini untuk mengambil scriptnya.</div>
-        <div class="mt-2 text-xs text-gray-500">★★★☆☆</div>
+        <div class="mt-2 text-xs text-gray-500">★★★☆☆ &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 0</div>
       </div>
     </a>
+
+    <!-- Project: FdF -->
+    <a
+      href="https://e-z.bio/marv"
+      target="_blank"
+      class="flex items-start gap-3 px-4 py-3 bg-[#202020]/30 border border-[#504945] rounded-lg text-sm hover:bg-[#2a2a2a] transition"
+    >
+      <div class="w-10 h-10 rounded-full bg-[#333] flex items-center justify-center text-xl">
+        🧱
+      </div>
+
+      <div>
+        <div class="font-bold text-lg">Project ketiga</div>
+        <div class="text-gray-300">ga tau males gua.</div>
+        <div class="mt-2 text-xs text-gray-500">★☆☆☆☆ &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 0</div>
+      </div>
+    </a>
+
+    <!-- Project: cub3D -->
+    <a
+      href="https://e-z.bio/marv"
+      target="_blank"
+      class="flex items-start gap-3 px-4 py-3 bg-[#202020]/30 border border-[#504945] rounded-lg text-sm hover:bg-[#2a2a2a] transition"
+    >
+      <div class="w-10 h-10 rounded-full bg-[#333] flex items-center justify-center text-xl">
+        🎮
+      </div>
+
+      <div>
+        <div class="font-bold text-lg">Malas</div>
+        <div class="text-gray-300">Kalcer abis.</div>
+        <div class="mt-2 text-xs text-gray-500">☆☆☆☆ &nbsp;&nbsp;|&nbsp;&nbsp; 🍴 1</div>
+      </div>
+    </a>
+  </div>
 </template>
